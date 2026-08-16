5c776adcb9734802928143a4eb0516db94c7f91d
juan4298jose <juanjosediazlop8@gmail.com>
2026-08-16 15:53:41 -0500

    correccion clase main

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         
         Grafo grafo = new Grafo();
+
         grafo.agregarArista(1, 2, 4);
         grafo.agregarArista(1, 3, 2);
         grafo.agregarArista(2, 4, 5);
@@ -10,5 +11,17 @@ public class Main {
         grafo.agregarArista(4, 5, 2);
 
         System.out.println("prueba de camino. :3");
-    } 
+
+        System.out.print("BFS partiendo desde el nodo 1: ");
+        
+        System.out.println();
+
+        System.out.print("DFS partiendo desde el nodo 1: ");
+        
+        System.out.println();
+
+        System.out.println("\n--- Distancias mínimas con Dijkstra desde el nodo 1 ---");
+ 
+    }
 }
+
